816bb08bd21b11fa28421d0a7153096ed0aa87f3
Georgi Ivanov <georgiivanov@Georgis-Mac.local>
2013-10-31 12:22:01 +0200

    Initial Commit

---
+++ CoursesManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoursesManager
+//
+//  Created by Georgi Ivanov on 10/31/13.
+//  Copyright (c) 2013 Georgi Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CoursesManagerTests/CoursesManagerTests.m
@@ -0,0 +1,34 @@
+//
+//  CoursesManagerTests.m
+//  CoursesManagerTests
+//
+//  Created by Georgi Ivanov on 10/31/13.
+//  Copyright (c) 2013 Georgi Ivanov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CoursesManagerTests : XCTestCase
+
+@end
+
+@implementation CoursesManagerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
